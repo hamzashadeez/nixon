3d2dca6f7432d877ba80269c6a69b8e001301cf9
hamzashadeez <hahmad1178@gmail.com>
2021-04-07 08:13:00 +0100

    firebase

--- src/Components/AddQuestion/AddQuestionModal.js
+++ src/Components/AddQuestion/AddQuestionModal.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import "./style.css";
+import { db } from "../../Data/firebase";
+import { v4 as uuidv4 } from "uuid";
 
-function AddQuestionModal({close, ...props}) {
+function AddQuestionModal({ close, ...props }) {
+  const [QuestionLabel, setQuestionLabel] = useState("");
+  const [option1, setOption1] = useState("");
+  const [option2, setOption2] = useState("");
+  const [option3, setOption3] = useState("");
+  const [option4, setOption4] = useState("");
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  const submit = async (e) => {
+    e.preventDefault();
+    const data = {
+      QuestionLabel,
+      option1,
+      option2,
+      option3,
+      option4,
+      correctAnswer,
+    };
+    if(correctAnswer !== ""){
+        db.collection("questions")
+          .doc(uuidv4())
+          .set(data)
+          .then(() => {
+            close();
+          })
+          .catch((err) => console.error(err));
+        console.log(data);
+
+    }else{
+        alert("Please Check the correct Answer")
+    }
+  };
+
   return (
     <Modal
       {...props}
@@ -12,28 +45,93 @@ function AddQuestionModal({close, ...props}) {
     >
       <Modal.Body className="modalx">
         <h4 className="text-info text-center">Add A Question</h4>
-        <form>
+        <form onSubmit={submit}>
           <label className="form_label text-info">Question label here</label>
-          <input className="form__input" required placeholder="Question label" />
+          <input
+            className="form__input"
+            value={QuestionLabel}
+            onChange={(e) => setQuestionLabel(e.target.value)}
+            required
+            placeholder="Question label"
+          />
+          <p style={{ color: "orange", fontSize: "12px" }}>
+            Make sure you check the correct answer before submitting
+          </p>
           <div className="obj_options">
-            <input className="" type="radio" name="rightAnswer" />
-            <input className="form__input" required placeholder="Option 1" />
+            <input
+              className=""
+              type="radio"
+              value={option1}
+              onChange={() => setCorrectAnswer(option1)}
+              name="rightAnswer"
+            />
+            <input
+              className="form__input"
+              value={option1}
+              onChange={(e) => setOption1(e.target.value)}
+              required
+              placeholder="Option 1"
+            />
           </div>
           <div className="obj_options">
-            <input className="" type="radio" name="rightAnswer" />
-            <input className="form__input" required placeholder="Option 2" />
+            <input
+              className=""
+              type="radio"
+              name="rightAnswer"
+              value={option2}
+              onChange={() => setCorrectAnswer(option2)}
+            />
+            <input
+              className="form__input"
+              required
+              placeholder="Option 2"
+              value={option2}
+              onChange={(e) => setOption2(e.target.value)}
+            />
           </div>
           <div className="obj_options">
-            <input className="" type="radio" name="rightAnswer" />
-            <input className="form__input" required placeholder="Option 3" />
+            <input
+              className=""
+              type="radio"
+              name="rightAnswer"
+              value={option2}
+              onChange={() => setCorrectAnswer(option2)}
+            />
+            <input
+              className="form__input"
+              required
+              placeholder="Option 3"
+              value={option3}
+              onChange={(e) => setOption3(e.target.value)}
+            />
           </div>
           <div className="obj_options">
-            <input className="" type="radio" name="rightAnswer" />
-            <input className="form__input" required placeholder="Option 4" />
+            <input
+              className=""
+              type="radio"
+              name="rightAnswer"
+              value={option2}
+              onChange={() => setCorrectAnswer(option2)}
+            />
+            <input
+              className="form__input"
+              required
+              placeholder="Option 4"
+              value={option4}
+              onChange={(e) => setOption4(e.target.value)}
+            />
           </div>
-          <div className='mt-3'>
-            <button className='btn btn-info mr-3' type='submit'>Submit</button>
-            <button className='btn btn-danger' type='button' onClick={()=>close()}>Cancel</button>
+          <div className="mt-3">
+            <button className="btn btn-info mr-3" type="submit">
+              Submit
+            </button>
+            <button
+              className="btn btn-danger"
+              type="button"
+              onClick={() => close()}
+            >
+              Cancel
+            </button>
           </div>
         </form>
       </Modal.Body>
--- src/Components/Questions/index.js
+++ src/Components/Questions/index.js
@@ -1,8 +1,25 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
-import AddQuestionModal from '../AddQuestion/AddQuestionModal'
+import AddQuestionModal from "../AddQuestion/AddQuestionModal";
+import { db } from "../../Data/firebase";
 
-function Questions({changeScreen}) {
+function Questions({ changeScreen }) {
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    (async () => {
+      db.collection("questions").onSnapshot((snapshot) => {
+        setQuestions(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+        console.log(questions);
+      });
+    })();
+
+    // questions.map((d) => console.log(d));
+  }, []);
   const d = [
     ["What is my name ?", "Usman", "Mohammad", "Hamza", "Sulaiman", "Hamza"],
     [
@@ -27,28 +44,30 @@ function Questions({changeScreen}) {
   ];
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
-  const closeModal = ()=>{
-    changeScreen(2)
-      setShow(false)
-  }
+  const closeModal = () => {
+    changeScreen(2);
+    setShow(false);
+  };
   return (
     <div className="questions__main_div">
-        <AddQuestionModal
-            show={show}
-            onHide={handleClose}
-            close={closeModal}
-        />
+      <AddQuestionModal show={show} onHide={handleClose} close={closeModal} />
       <div className="list_of_questions">
-        {d.map((data, index) => (
+        {/* {d.map((data, index) => (
           <h2 id="qest" key={data[0]}>
             {index + 1}. {data[0]}
           </h2>
+          QuestionLabel: "The Capital of Nigeria"
+
+        ))} */}
+        {questions.map(({ id, data }) => (
+          <p key={id}>* {data.QuestionLabel}</p>
         ))}
       </div>
       <div className="option__side">
-        <div className='add_a_question_div'>
-          <button onClick={()=>setShow(true)} className='btn btn-info'>Add a Question</button>
-          
+        <div className="add_a_question_div">
+          <button onClick={() => setShow(true)} className="btn btn-info">
+            Add a Question
+          </button>
         </div>
       </div>
     </div>
